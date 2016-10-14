79979ac444d103f7acdc96a6671e0dce2a07cddb
Brett Hoerner <brett@bretthoerner.com>
2016-10-14 10:24:54 -0500

    Test new filters.

--- raven-java-log4j2-example/src/main/java/com/getsentry/example/Application.java
+++ raven-java-log4j2-example/src/main/java/com/getsentry/example/Application.java
@@ -5,10 +5,14 @@ import org.apache.logging.log4j.Logger;
 
 public class Application
 {
+    private static final Logger ravenLogger = LogManager.getLogger("com.getsentry.raven.RavenLogger");
+    
     private static final Logger logger = LogManager.getLogger(Application.class);
 
     public static void main(String[] args)
     {
+        ravenLogger.error("This should not be sent to Sentry, because it originates inside of Raven itself.");
+
         try {
             int example = 1 / 0;
         } catch (Exception e) {
--- raven-java-logback-example/src/main/java/com/getsentry/example/Application.java
+++ raven-java-logback-example/src/main/java/com/getsentry/example/Application.java
@@ -5,10 +5,14 @@ import org.slf4j.LoggerFactory;
 
 public class Application
 {
+    private static final Logger ravenLogger = LoggerFactory.getLogger("com.getsentry.raven.RavenLogger");
+
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args)
     {
+        ravenLogger.error("This should not be sent to Sentry, because it originates inside of Raven itself.");
+        
         try {
             int example = 1 / 0;
         } catch (Exception e) {
